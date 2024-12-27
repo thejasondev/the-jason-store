c3331674bf0f787549003e4b98159f42f975c479
thejasondev <jason.guerra1002@gmail.com>
2024-12-27 18:25:56 -0500

    cambios estruct

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,8 +3,8 @@ import logo from "../assets/Logos/logo.png";
 
 const Home = () => {
   return (
-    <section className="flex flex-row items-center relative ml-20 mt-10 lg:mt-0 mb-10">
-      <div className="w-1/2">
+    <section className="items-center relative ml-20 mt-10 lg:mt-0 mb-10">
+      <div className="w-auto lg:w-1/2 sm:ml-10">
         <h1 className="text-xl lg:text-4xl font-bold mb-4">
           Bienvenido a Nuestra Tienda
         </h1>
@@ -16,21 +16,19 @@ const Home = () => {
         <h3 className="text-base lg:text-xl text-gray-600 mb-5">
           Somos un TODO en UNO!.
         </h3>
-
+        <div className="w-auto lg:w-1/2 sm:ml-10"> 
+          <img
+            src={logo}
+            alt="Logo TheJasonSTORE"
+            className="w-50 h-auto object-cover rounded-full hover:shadow-lg"
+          />
+        </div>
         <Link
           to="/discounts"
           className="rounded cursor-pointer p-2 mx-10 bg-black text-white font-semibold"
         >
           Descuentos
         </Link>
-      </div>
-
-      <div className="w-1/2">
-        <img
-          src={logo}
-          alt="Logo TheJasonSTORE"
-          className="w-50 h-auto object-cover rounded-full hover:shadow-lg"
-        />
       </div>
     </section>
   );
