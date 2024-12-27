0029975bd39c861c4274b8a1e594fccd0520d78c
thejasondev <jason.guerra1002@gmail.com>
2024-12-27 16:35:12 -0500

    viteconfig

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   base: "/the-jason-store/",
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: "index.html",
+    },
+  },
 });
