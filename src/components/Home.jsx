3f8bdfd0010b02c9691de4156462642ade3e9099
thejasondev <jason.guerra1002@gmail.com>
2024-12-27 18:31:47 -0500

    estruct

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,7 +4,7 @@ import logo from "../assets/Logos/logo.png";
 const Home = () => {
   return (
     <section className="items-center relative ml-20 mt-10 lg:mt-0 mb-10">
-      <div className="w-auto lg:w-1/2 sm:ml-10">
+      <div className="w-auto">
         <h1 className="text-xl lg:text-4xl font-bold mb-4">
           Bienvenido a Nuestra Tienda
         </h1>
@@ -16,11 +16,11 @@ const Home = () => {
         <h3 className="text-base lg:text-xl text-gray-600 mb-5">
           Somos un TODO en UNO!.
         </h3>
-        <div className="w-auto lg:w-1/2 sm:ml-10"> 
+        <div className="mt-5 mb-5">
           <img
             src={logo}
             alt="Logo TheJasonSTORE"
-            className="w-50 h-auto object-cover rounded-full hover:shadow-lg"
+            className="w-40 h-auto mx-auto object-cover rounded-full hover:shadow-lg"
           />
         </div>
         <Link
