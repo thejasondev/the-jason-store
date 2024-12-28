60cadd77a41e517514790d6f7e85dcb980f9ef26
thejasondev <jason.guerra1002@gmail.com>
2024-12-27 19:10:38 -0500

    big changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,23 +1,27 @@
 import { Link } from "react-router-dom";
+import logo from "../assets/Logos/logo.png";
 
 const Home = () => {
   return (
-    <section className="items-center relative ml-20 mt-10 lg:mt-0 mb-10">
-      <div className="w-auto">
-        <h1 className="text-xl lg:text-4xl font-bold mb-4">
-          Bienvenido a Nuestra Tienda
-        </h1>
-        <p className="text-base lg:text-xl text-gray-600 mb-5">
+    <section className="relative py-12 bg-white">
+      <div className="container mx-auto text-center">
+        <h1 className="text-3xl font-bold mb-6">Bienvenido a Nuestra Tienda</h1>
+        <p className="text-gray-600 mb-6">
           Nuestra tienda es un lugar donde encontrarás las mejores ofertas en
           tecnología, ropa, comida y mucho más. Ofrecemos productos de alta
           calidad con descuentos exclusivos.
         </p>
-        <h3 className="text-base lg:text-xl text-gray-600 mb-5">
-          Somos un TODO en UNO!.
-        </h3>
+        <h3 className="text-gray-600 mb-6">Somos un TODO en UNO!.</h3>
+        <div className="mt-5 mb-5">
+          <img
+            src={logo}
+            alt="Logo TheJasonSTORE"
+            className="mx-auto w-40 h-auto rounded-full"
+          />
+        </div>
         <Link
           to="/discounts"
-          className="rounded cursor-pointer p-2 mx-10 bg-black text-white font-semibold"
+          className="flex items-center justify-center px-6 py-3 rounded-lg cursor-pointer bg-black text-white font-semibold"
         >
           Descuentos
         </Link>
