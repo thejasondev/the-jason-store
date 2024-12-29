2df6e27667901e731adfcc5fb85ebe95b67fa712
thejasondev <jason.guerra1002@gmail.com>
2024-12-29 14:09:37 -0500

    prep depl vercel

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  base: import.meta.env.VITE_GITHUB_PAGES === "true" ? "/thejasonstore/" : "/",
-  rollupOptions: {
-    input: "index.html",
-  },
+export default defineConfig(({ mode }) => {
+  
+  const base = mode === "gh-pages" ? "/the-jason-store/" : "/";
+
+  return {
+    plugins: [react()],
+    base: base,
+    rollupOptions: {
+      input: "index.html",
+    },
+  };
 });
